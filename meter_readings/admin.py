e37517b073dc90b8022c537905f97086ed819722
kttyo <16376462+kttyo@users.noreply.github.com>
2023-02-21 14:23:57 +0900

    Specify fields to display in the admin site

--- meter_readings/admin.py
+++ meter_readings/admin.py
@@ -4,8 +4,9 @@ from .models import Files, RegisterReadings
 
 @admin.register(Files)
 class FilesAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('file_name', 'header', 'footer', 'ingestion_time')
+
 
 @admin.register(RegisterReadings)
 class RegisterReadingsAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('mpan_core', 'meter_id', 'meter_register_id', 'reading_date_time', 'register_reading', 'md_reset_date_time', 'number_of_md_resets', 'meter_reading_flag', 'reading_method', 'file_name', 'ingestion_time')
