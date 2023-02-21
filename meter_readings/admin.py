fd02351c32fd958927d65d47216419a0cf56b6c1
kttyo <16376462+kttyo@users.noreply.github.com>
2023-02-21 14:02:32 +0900

    Register models to admin site

--- meter_readings/admin.py
+++ meter_readings/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import Files, RegisterReadings
 # Register your models here.
+
+@admin.register(Files)
+class FilesAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(RegisterReadings)
+class RegisterReadingsAdmin(admin.ModelAdmin):
+    pass
