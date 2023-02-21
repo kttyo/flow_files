eed048c990a8913f17c654f033763e69e10a6cd9
kttyo <16376462+kttyo@users.noreply.github.com>
2023-02-21 13:34:00 +0900

    Create Files model

--- meter_readings/models.py
+++ meter_readings/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Files(models.Model):
+    file_name = models.CharField(max_length=100)
+    header = models.CharField(max_length=100)
+    footer = models.CharField(max_length=100)
+    ingestion_time = models.DateTimeField()
+
