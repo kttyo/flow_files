f9e0dda82ffe1742c8fe6ed7baee03202abf31e1
kttyo <16376462+kttyo@users.noreply.github.com>
2023-02-24 09:37:38 +0900

    Create test

--- meter_readings/tests.py
+++ meter_readings/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from meter_readings.models import Files, RegisterReadings
+import os.path
+from django.conf import settings
+from django.core.management import call_command
 
 # Create your tests here.
+class DataTestCase(TestCase):
+    def test_data_comparison(self):
+        call_command('file_ingestion', 'meter_readings/file_inbox/DTC5259515123502080915D0010.uff')
+        data_count = RegisterReadings.objects.all().count()
+        self.assertEqual(data_count,13)
