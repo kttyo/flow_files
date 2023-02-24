93dc8dbe24c7ac91144f0ea12433bb7efb597278
kttyo <16376462+kttyo@users.noreply.github.com>
2023-02-24 13:08:03 +0900

    Process all files in the inbox folder unless specific file name is given via the command argument

--- meter_readings/management/commands/file_ingestion.py
+++ meter_readings/management/commands/file_ingestion.py
@@ -4,7 +4,10 @@ from meter_readings.models import Files, RegisterReadings
 import os.path
 import os
 from datetime import datetime
+from django.utils import timezone
 
+file_inbox_path = settings.BASE_DIR / 'meter_readings/file_inbox/'
+imported_files_path = settings.BASE_DIR / 'meter_readings/file_inbox/imported_files/'
 
 def rr_object(mpan_cores, meter_reading_type, register_readings, file_name, ingestion_time):
     new_object = RegisterReadings(
@@ -29,71 +32,80 @@ def rr_object(mpan_cores, meter_reading_type, register_readings, file_name, inge
     return new_object
 
 
+def create_rr_object(file_listified, ingestion_time, file_name):
+    object_list = []
+    mpan_cores = []
+    meter_reading_type = []
+    register_readings = []
+
+    prev_record_type = '000'
+
+    for line in file_listified:
+        line_listified = line.split('|')
+
+        # Skip header
+        if line_listified[0] > '999' and not mpan_cores:
+            continue
+
+        # Create object and append it to object_list when a record set ends or footer appers
+        if line_listified[0] <= prev_record_type or line_listified[0] > '999':
+            new_rr_object = rr_object(mpan_cores, meter_reading_type, register_readings, file_name, ingestion_time)
+            object_list.append(new_rr_object)
+
+        # Fresh start of the flow file.
+        if line_listified[0] == '026':
+            mpan_cores = line_listified
+
+        # Continue scanning the record set for MPAN.
+        if line_listified[0] == '028':
+            meter_reading_type = line_listified
+
+        if line_listified[0] == '030':
+            register_readings = line_listified
+        
+        prev_record_type = line_listified[0]
+    try:    
+        RegisterReadings.objects.bulk_create(object_list)
+    except Exception as e:
+        print(e)
+    else:
+        os.rename(file_inbox_path / file_name, imported_files_path / file_name)
+
+
 class Command(BaseCommand):
     def add_arguments(self, parser):
-        parser.add_argument('file_path', nargs='?', default='')
+        parser.add_argument('file_name', nargs='?', default='')
 
 
     def handle(self, *args, **options):
-        file_path = options.get('file_path')
-        file_name = os.path.basename(options.get('file_path'))
-        ingestion_time = datetime.now()
-        imported_files_path = settings.BASE_DIR / 'meter_readings/file_inbox/imported_files/'
+        ingestion_time = timezone.now()
 
-        try:
-            with open(file_path,'r') as file:
-                file_listified = list(file)
-        except Exception as e:
-            print(e)
+        # By default, all .uff files in the inbox are targeted for processing
+        paths = os.listdir(file_inbox_path)
+        uff_files = [path for path in paths if path.endswith('.uff')]
 
-        # Proceed only when the file has not been loaded before
-        existing_file = Files.objects.filter(file_name=file_name)
-        if existing_file.count() > 0:
-            print('The file you requested to ingest is already present in the database.')
-            return
+        # If file name is passed as a command argument, only that file gets processed
+        if options.get('file_name'):
+            uff_files = [options.get('file_name')]
 
-        Files.objects.create(
-            file_name=file_name,
-            header=file_listified[0],
-            footer=file_listified[-1],
-            ingestion_time=ingestion_time
-        )
+        for file_name in uff_files:    
+            try:
+                with open(file_inbox_path / file_name,'r') as file:
+                    file_listified = list(file)
+            except Exception as e:
+                print(e)
 
+            # Proceed only when the file has not been loaded before
+            existing_file = Files.objects.filter(file_name=file_name)
+            if existing_file.count() > 0:
+                print('The file you requested to ingest is already present in the database.')
+                return
 
-        object_list = []
-        mpan_cores = []
-        meter_reading_type = []
-        register_readings = []
+            Files.objects.create(
+                file_name=file_name,
+                header=file_listified[0],
+                footer=file_listified[-1],
+                ingestion_time=ingestion_time
+            )
 
-        prev_record_type = '000'
-
-        for line in file_listified:
-            line_listified = line.split('|')
-
-            # Skip header
-            if line_listified[0] > '999' and not mpan_cores:
-                continue
-
-            # Create object and append it to object_list when a record set ends or footer appers
-            if line_listified[0] <= prev_record_type or line_listified[0] > '999':
-                new_rr_object = rr_object(mpan_cores, meter_reading_type, register_readings, file_name, ingestion_time)
-                object_list.append(new_rr_object)
-
-            # Fresh start of the flow file.
-            if line_listified[0] == '026':
-                mpan_cores = line_listified
-
-            # Continue scanning the record set for MPAN.
-            if line_listified[0] == '028':
-                meter_reading_type = line_listified
-
-            if line_listified[0] == '030':
-                register_readings = line_listified
-            
-            prev_record_type = line_listified[0]
-        try:    
-            RegisterReadings.objects.bulk_create(object_list)
-        except Exception as e:
-            print(e)
-        else:
-            os.rename(file_path,imported_files_path / file_name)
+            create_rr_object(file_listified, ingestion_time, file_name)
