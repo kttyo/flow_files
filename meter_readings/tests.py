65c2fb907b58ecdbba4453bec666082bac3ce422
kttyo <16376462+kttyo@users.noreply.github.com>
2023-02-24 11:54:35 +0900

    Move the file back to inbox

--- meter_readings/tests.py
+++ meter_readings/tests.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 from meter_readings.models import RegisterReadings
 from django.core.management import call_command
+import os
+
 
 # Create your tests here.
 class DataTestCase(TestCase):
@@ -13,5 +15,9 @@ class DataTestCase(TestCase):
             if line.split('|')[0] == '030':
                 count_rr_record += 1
         call_command('file_ingestion', './meter_readings/file_inbox/DTC5259515123502080915D0010.uff')
+        os.rename(
+            './meter_readings/file_inbox/imported_files/DTC5259515123502080915D0010.uff',
+            './meter_readings/file_inbox/DTC5259515123502080915D0010.uff'
+            )
         data_count = RegisterReadings.objects.all().count()
         self.assertEqual(data_count,count_rr_record)
