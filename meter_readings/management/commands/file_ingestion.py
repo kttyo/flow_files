cce1fc0198046221a6e65f9c8f5c760c27866d37
kttyo <16376462+kttyo@users.noreply.github.com>
2023-02-24 09:51:00 +0900

    Simplify the logic in handle()

--- meter_readings/management/commands/file_ingestion.py
+++ meter_readings/management/commands/file_ingestion.py
@@ -5,19 +5,19 @@ import os.path
 from datetime import datetime
 
 
-def register_readings_object(file_name, current_time, field_values):
+def create_object(mpan_cores, meter_reading_type, register_readings, file_name, ingestion_time):
     rr_object = RegisterReadings(
-        mpan_core = field_values['mpan_core'],
-        meter_id = field_values['meter_id'],
-        meter_register_id = field_values['meter_register_id'],
-        reading_date_time = datetime.strptime(field_values['reading_date_time'],'%Y%m%d%H%M%S'),
-        register_reading = field_values['register_reading'],
-        md_reset_date_time = None if field_values['md_reset_date_time'] == '' else datetime.strptime(field_values['md_reset_date_time'],'%Y%m%d%H%M%S'),
-        number_of_md_resets = None if field_values['number_of_md_resets'] == '' else field_values['number_of_md_resets'],
-        meter_reading_flag = True if field_values['meter_reading_flag'] == 'T' else False,
-        reading_method = field_values['reading_method'],
+        mpan_core = mpan_cores[1],
+        meter_id = meter_reading_type[1],
+        meter_register_id = register_readings[1],
+        reading_date_time = datetime.strptime(register_readings[2],'%Y%m%d%H%M%S'),
+        register_reading = register_readings[3],
+        md_reset_date_time = None if register_readings[4] == '' else datetime.strptime(register_readings[4],'%Y%m%d%H%M%S'),
+        number_of_md_resets = None if register_readings[5] == '' else register_readings[5],
+        meter_reading_flag = True if register_readings[6] == 'T' else False,
+        reading_method = register_readings[7],
         file_name = file_name,
-        ingestion_time = current_time
+        ingestion_time = ingestion_time
     )
 
     return rr_object
@@ -33,7 +33,7 @@ class Command(BaseCommand):
             file_listified = list(file)
 
         file_name = os.path.basename(options.get('file_path'))
-        current_time = datetime.now()
+        ingestion_time = datetime.now()
 
         # Proceed only when the file has not been loaded before
         existing_file = Files.objects.filter(file_name=file_name)
@@ -44,45 +44,40 @@ class Command(BaseCommand):
             file_name=file_name,
             header=file_listified[0],
             footer=file_listified[-1],
-            ingestion_time=current_time
+            ingestion_time=ingestion_time
         )
 
 
         object_list = []
-        field_values = {}
+        mpan_cores = []
+        meter_reading_type = []
+        register_readings = []
 
-        for i in range(1,len(file_listified)-1):
-            record = file_listified[i]
-            record_listified = record.split('|')
+        prev_record_type = '000'
 
-            # Repeating record for Meter/Register. Create an object using the previous Meter/Register before continuing.
-            if 'meter_register_id' in field_values and record_listified[0] == '030':
-                the_object = register_readings_object(file_name, current_time, field_values)
-                object_list.append(the_object)
+        for line in file_listified:
+            line_listified = line.split('|')
 
-            # Repeating record for MPAN Core. Create an object with the previous record set and restart the process. 
-            if 'mpan_core' in field_values and record_listified[0] == '026':
-                the_object = register_readings_object(file_name, current_time, field_values)
-                object_list.append(the_object)
+            # Skip header
+            if line_listified[0] > '999' and not mpan_cores:
+                continue
 
-                field_values = {}
-                field_values['mpan_core'] = record_listified[1]
+            # Create object and append it to object_list when a record set ends or footer appers
+            if line_listified[0] <= prev_record_type or line_listified[0] > '999':
+                new_object = create_object(mpan_cores, meter_reading_type, register_readings, file_name, ingestion_time)
+                object_list.append(new_object)
 
             # Fresh start of the flow file.
-            if 'mpan_core' not in field_values and record_listified[0] == '026':
-                field_values['mpan_core'] = record_listified[1]
+            if line_listified[0] == '026':
+                mpan_cores = line_listified
 
             # Continue scanning the record set for MPAN.
-            if record_listified[0] == '028':
-                field_values['meter_id'] = record_listified[1]
+            if line_listified[0] == '028':
+                meter_reading_type = line_listified
 
-            if record_listified[0] == '030':
-                field_values['meter_register_id'] = record_listified[1]
-                field_values['reading_date_time'] = record_listified[2]
-                field_values['register_reading'] = record_listified[3]
-                field_values['md_reset_date_time'] = record_listified[4]
-                field_values['number_of_md_resets'] = record_listified[5]
-                field_values['meter_reading_flag'] = record_listified[6]
-                field_values['reading_method'] = record_listified[7]
+            if line_listified[0] == '030':
+                register_readings = line_listified
+            
+            prev_record_type = line_listified[0]
             
         RegisterReadings.objects.bulk_create(object_list)
