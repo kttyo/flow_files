aad09d81305cd9e124dbcf8e1728bf5170847447
kttyo <16376462+kttyo@users.noreply.github.com>
2023-02-24 10:35:37 +0900

    Automatically get the count of 030 records from the file

--- meter_readings/tests.py
+++ meter_readings/tests.py
@@ -1,12 +1,17 @@
 from django.test import TestCase
-from meter_readings.models import Files, RegisterReadings
-import os.path
-from django.conf import settings
+from meter_readings.models import RegisterReadings
 from django.core.management import call_command
 
 # Create your tests here.
 class DataTestCase(TestCase):
     def test_data_comparison(self):
-        call_command('file_ingestion', 'meter_readings/file_inbox/DTC5259515123502080915D0010.uff')
+        with open('./meter_readings/file_inbox/DTC5259515123502080915D0010.uff', 'r') as file:
+            file_listified = file.readlines()
+        
+        count_rr_record = 0
+        for line in file_listified:
+            if line.split('|')[0] == '030':
+                count_rr_record += 1
+        call_command('file_ingestion', './meter_readings/file_inbox/DTC5259515123502080915D0010.uff')
         data_count = RegisterReadings.objects.all().count()
-        self.assertEqual(data_count,13)
+        self.assertEqual(data_count,count_rr_record)
